a50e5b2fb83db13d04025167ca9bf7f7ad3d685d
wk-j <somnuk.wk@gmail.com>
2017-08-14 01:46:53 +0700

    Added click handler

--- src/deep_end_reagent/core.cljs
+++ src/deep_end_reagent/core.cljs
@@ -5,13 +5,38 @@
 ;; -------------------------
 ;; Views
 
+(def videos
+      [{:source "https://www.youtube.com/embed/NCXfKyfpBKI?ecver=1&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1" :title "Deep End"}
+       {:source "https://www.youtube.com/embed/lBN9VDFDvOk?ecver=1&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1" :title "Heart Stop"}
+       {:source "https://www.youtube.com/embed/zeP7bqMySmE?ecver=1&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1" :title "Strangled Love"}])
+
+(def current-video (r/atom (get videos 0)))
+
+(defn play [video]
+  #(swap! current-video (fn [] video)))
+
+(defn item [video]
+  [:li.list-group-item {:key (get video :source) :on-click (play video)}
+    [:img.img-circle.media-object.pull-left {:src "images/large-round.png" :width 32 :height 32}]
+    [:div.media-body
+      [:strong (get video :title)]
+      [:p "Jannine Weigel"]]])
+
 (defn deep-end []
       [:div.window
-        [:header.footbar.toolbar-header
+        [:header.toolbar.toolbar-header
           [:div.toolbar-actions
             [:button.btn.btn-default.pull-right
-              [:span.icon.icon-facebook]]]]])
-        
+              [:span.icon.icon-facebook]]]]
+        [:div.window-content 
+          [:div.pane {:style {:overflow-y "visible" :border-left "none"}}
+            [:ul.list-group
+              (map item videos)]]
+          [:div.pane
+            [:strong (get @current-video :source)]
+            [:iframe {:style {:border 0}} :title "video" :width 550 :height 315 :src (get @current-video :source)]]]
+        [:footer.toolbar.toolbar-footer
+          [:h1.title]]])
 
 (defn home-page []
   [:div 
